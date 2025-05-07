86aed21a40b0992d9bcd9fa95d9c2de1a1a44b7b
James <0rd1nary2333@gmail.com>
2025-05-06 23:29:42 -0700

    update js and json

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,35 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const hornAudio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+
+  hornSelect.addEventListener('change', () => {
+    const selectedHorn = hornSelect.value;
+    hornImage.src = `assets/images/${selectedHorn}.svg`;
+    hornAudio.src = `assets/audio/${selectedHorn}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = volumeSlider.value;
+    hornAudio.volume = volume / 100;
+    if (vol === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (vol < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (vol < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }  
+  });
+  
+  playButton.addEventListener('click', () => {
+    hornAudio.play();
+  });
+  
 }
